66d2bd5e7b2ac34a109503a78be8e080660f3d62
Katy Yut <katherine.yut@ou.edu>
2022-03-15 14:25:19 -0500

    Tweaks, playing around

--- initial_code.R
+++ initial_code.R
@@ -5,3 +5,14 @@ api_key <- "AIzaSyAxVOsu2QBilGHU8WcAbJXc3Wchh8HayFQ"
 
 data <- read.csv("airports.csv")
 head(data)
+
+# Add coloumns with information
+data$COLOR <- "lavender"
+data$INFO <- paste0(data$AIRPORT, " | ", data$CITY, ", ", data$STATE)
+
+map <- google_map(
+  key = api_key,
+  data = data
+)
+map %>%
+  add_markers(lat = "LATITUDE", lon = "LONGITUDE", mouse_over="IATA")
